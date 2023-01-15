a9649fd83563748fd08c8ee735e15651692b2643
Jonathan Liera <jonliera1@gmail.com>
2023-01-14 18:25:52 -0800

    refactor models machine.cs to customize entity properties with data annotations

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Machine
   {
     public int MachineId { get; set; }
+    [Required(ErrorMessage = "This field can't be empty")]
     public string Name { get; set; }
-    public List<EngineerMachine> JoinEntities { get; set;}
+    public int EngineerId { get; set; }
+    public List<EngineerMachine> JoinEntities { get; }
   }
 }
